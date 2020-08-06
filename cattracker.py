01a19b02d08bf53b6c9e6db6bbae7033ba08158a
Kelly <kbell109@gmail.com>
2020-08-06 19:29:04 -0400

    Initial commit

---
+++ cattracker.py
@@ -0,0 +1,84 @@
+from datetime import datetime
+from enum import Enum
+
+import gspread
+from evdev import InputDevice, ecodes
+from oauth2client.service_account import ServiceAccountCredentials
+
+
+SCOPES = [
+    "https://spreadsheets.google.com/feeds",
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+]
+
+
+SHEET_NAME = "NEW Cat Tracker 2020"
+
+
+PUMPKIN_PEE_COLUMN = 1
+PUMPKIN_POO_COLUMN = 2
+PUMPKIN_PUKE_COLUMN = 3
+PATCH_PEE_COLUMN = 4
+PATCH_POO_COLUMN = 5
+PATCH_PUKE_COLUMN = 6
+
+
+GAMEPAD = InputDevice("/dev/input/event0")
+
+
+class Button(Enum):
+    X = 288
+    A = 289
+    B = 290
+    Y = 291
+    SELECT = 296
+    START = 297
+
+
+def get_current_month():
+    now = datetime.now()
+    return now.strftime("%B")
+
+def get_current_sheet():
+    client = login()
+    month = get_current_month()
+    return client.open(SHEET_NAME).worksheet(month)
+
+def get_time_string():
+    return datetime.now().strftime("%m/%d/%Y %I:%M %p")
+
+def login():
+    creds = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", SCOPES)
+    return gspread.authorize(creds)
+
+def log(col):
+    sheet = get_current_sheet()
+    row = len(sheet.col_values(col)) + 1
+    sheet.update_cell(row, col, get_time_string())
+
+
+def main():
+
+    BUTTON_MAPPING = {
+        Button.Y.value: (PUMPKIN_PEE_COLUMN, "Pumpkin peed"),
+        Button.X.value: (PUMPKIN_POO_COLUMN, "Pumpkin pooed"),
+        Button.SELECT.value: (PUMPKIN_PUKE_COLUMN, "Pumpkin puked"),
+        Button.B.value: (PATCH_PEE_COLUMN, "Patch peed"),
+        Button.A.value: (PATCH_POO_COLUMN, "Patch pooed"),
+        Button.START.value: (PATCH_PUKE_COLUMN, "Patch puked"),
+    }
+
+    for event in GAMEPAD.read_loop():
+        if event.type == ecodes.EV_KEY and event.value:
+            col, log_msg = BUTTON_MAPPING.get(event.code, (0,""))
+
+            if not col:
+                continue
+
+            log(col)
+            print(log_msg)
+
+
+main()
